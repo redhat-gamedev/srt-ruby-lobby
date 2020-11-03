ccfeeb700a1a4be6eccd05ef37fe7b1b7ca92c66
Erik M Jacobs <erikmjacobs@gmail.com>
2020-11-03 12:52:43 -0500

    updates openshift manifests
    * changes to put auth into srt project
    * changes created project to be srt
    * changes script for new files

--- openshift-manifests/deploy.sh
+++ openshift-manifests/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-oc create -f auth-project.yaml
+oc create -f project.yaml
 oc create -f rhsso-operatorgroup.yaml
 oc create -f rhsso-operator-subscription.yaml
 oc create -f rhsso-keycloak-cr.yaml

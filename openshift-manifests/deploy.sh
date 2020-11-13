5fad64d93b9a48e74b9ecdd265decf865e7baca0
Erik M Jacobs <erikmjacobs@gmail.com>
2020-11-13 12:58:13 -0500

    updates deploy script to cover all manifests

--- openshift-manifests/deploy.sh
+++ openshift-manifests/deploy.sh
@@ -7,14 +7,15 @@ oc apply -f rhsso-operator-subscription.yaml
 oc apply -f amq-operator-subscription.yaml
 oc apply -f jdg-operator-subscription.yaml
 
-sleep 15
-
 # create the jdg secrets
 oc apply -f jdg-credentials-secret.yaml
 oc apply -f jdg-playerdata-credentials.yaml
 
+sleep 15
+
 # need to wait for the crds to exist
 oc apply -f rhsso-keycloak-cr.yaml
 oc apply -f amq-broker-cr.yaml
+oc apply -f amq-chat-address-cr.yaml
 oc apply -f jdg-cluster-cr.yaml
 oc apply -f jdg-playerdata-cache.yaml
